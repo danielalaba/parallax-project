ccc3c92351bab38598161aa134c7887a89464a8e
danielalaba <ralphalaba@gmail.com>
2024-07-22 12:30:20 +0800

    add parallax project

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,40 @@
+"use client"
 import Image from "next/image";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Parallax pages={8}>
+        <ParallaxLayer speed={0.3} className="flex items-center justify-center">
+          <h1 className="text-4xl font-extrabold">Scroll Down</h1>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5} className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-extrabold"> Simple Parallax Effect</h1>
+          <h2>Made by Daniel Alaba</h2>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.5} speed={1} sticky={{ start: 1, end: 5.9}} style={{ zIndex: 2 }}>
+          <img src="/img/house.png" className="w-screen h-auto" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.5} factor={1} speed={0.3} style={{backgroundImage: 'url(/img/stars.png)', backgroundSize: 'cover', zIndex: -1}} sticky={{start: 2, end: 3}}/>
+        <ParallaxLayer offset={2.5} speed={0.5} className="w-96 flex justify-start items-center ml-96">
+          <img src="/img/moon.png" className="w-80 h-auto"/>
+        </ParallaxLayer>
+        <ParallaxLayer offset={3} factor={2} speed={0.5} style={{ backgroundImage: 'url(/img/gradient.png)', backgroundSize: 'cover'}}/>
+        <ParallaxLayer offset={4.5} factor={2.5} speed={0.5} className="bg-white"/>
+        <ParallaxLayer offset={4.5} factor={2} speed={0.3} sticky={{start: 4.5, end: 5.5}} style={{ zIndex: 1 }}>
+          <img src="/img/clouds.png" className="w-96 h-auto absolute left-10 mt-10"/>
+          <img src="/img/clouds.png" className="w-96 h-auto absolute right-40 top-20 mt-10"/>
+        </ParallaxLayer>
+        <ParallaxLayer offset={5} speed={0.3} className="w-96 flex justify-start items-center ml-96">
+          <img src="/img/sun.png" className="w-80 h-auto"/>
+        </ParallaxLayer>
+        <ParallaxLayer offset={6} factor={2} speed={0.5} style={{ backgroundImage: 'url(/img/bnw.png)', backgroundSize: 'cover'}}/>
+        <ParallaxLayer offset={7} speed={0.3} className="flex justify-center items-center bg-black">
+          <h1 className="text-4xl font-extrabold">Thank You for Visiting! ^___^</h1>
+        </ParallaxLayer>
+      </Parallax>
     </main>
   );
 }
